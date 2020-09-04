12063a22f90d7fbf256bc5120d6c42450f8fe96e
wangjunzjhw <percy7@yeah.net>
2020-09-04 16:13:27 +0800

    首次提交

---
+++ basis/print/demo.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 测试输出函数
+print('A')
+print('B')
+print('C')
---
+++ dataType.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+python数据类型：
+版本2：
+    不可变：Number，Sring，Tuple(元素)
+    可变：List，Dictionary(字典)，Set(集合)
+版本3：Int，Float，Bool，Complex(复数)
+'''
+
+'''
+python数据类型表示：
+Tuple元祖用()表示，元祖可以包含List，List可以被改变
+List用[]表示
+String用''表示
+Number用数字表示
+Bool用True/False表示
+Float用浮点数表示
+Dictionary用{}表示，key：value方式存储数据
+'''
+
+'''
+常用函数：
+range(101)：生成一个小于101的整数列并转换成List
+'''
